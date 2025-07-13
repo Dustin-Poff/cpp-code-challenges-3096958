1b58c4443ee1dca1461468bcdcd15017c2c85c1c
Dustin-Poff <32342126+Dustin-Poff@users.noreply.github.com>
2025-07-13 04:26:04 +0000

    My tictactoe ai game.

--- src/10/Challenge/ch10_tictactoe.cpp
+++ src/10/Challenge/ch10_tictactoe.cpp
@@ -27,6 +27,165 @@ void ask_for_move(char game[][3], char mark){
     return;
 }
 
+bool try_to_win(char game[][3], char mark){
+    int mark_count = 0, empty_index = -1;
+
+    //checks for winning move
+    for (size_t i = 0; i < 3; i++)
+    {
+        mark_count = 0, empty_index = -1;
+        for (size_t j = 0; j < 3; j++)
+        {
+            if (game[i][j] == mark) mark_count ++;
+            else if(game[i][j] == ' ') empty_index = j;
+        }
+        if(mark_count == 2 && empty_index != -1)
+        {
+            game[i][empty_index] = mark;
+            return true;
+        }
+
+        mark_count = 0, empty_index = -1;
+        for (size_t j = 0; j < 3; j++)
+        {
+            if (game[j][i] == mark) mark_count ++;
+            else if(game[j][i] == ' ') empty_index = j;
+        }
+        if(mark_count == 2 && empty_index != -1)
+        {
+            game[empty_index][i] = mark;
+            return true;
+        }
+    }
+
+    mark_count = 0, empty_index = -1;
+    for (size_t i = 0; i < 3; i++)
+    {
+        if(game[i][i] == mark) mark_count ++;
+        else if (game[i][i] == ' ') empty_index = i;
+    }
+    if(mark_count == 2 && empty_index != -1)
+    {
+        game[empty_index][empty_index] = mark;
+        return true;
+    }
+
+    mark_count = 0, empty_index = -1;
+    for (size_t i = 0; i < 3; i++)
+    {
+        if(game[2 - i][i] == mark) mark_count ++;
+        else if (game[2 - i][i] == ' ') empty_index = i;
+    }
+    if(mark_count == 2 && empty_index != -1)
+    {
+        game[empty_index][empty_index] = mark;
+        return true;
+    }
+
+    return false;
+}
+
+bool try_to_block(char game[][3], char mark, char oppMark){
+    int oppmark_count = 0, empty_index = -1;
+
+    //checks for winning move
+    for (size_t i = 0; i < 3; i++)
+    {
+        oppmark_count = 0, empty_index = -1;
+        for (size_t j = 0; j < 3; j++)
+        {
+            if (game[i][j] == oppMark) oppmark_count ++;
+            else if(game[i][j] == ' ') empty_index = j;
+        }
+        if(oppmark_count == 2 && empty_index != -1)
+        {
+            game[i][empty_index] = mark;
+            return true;
+        }
+
+        oppmark_count = 0, empty_index = -1;
+        for (size_t j = 0; j < 3; j++)
+        {
+            if (game[j][i] == oppMark) oppmark_count ++;
+            else if(game[j][i] == ' ') empty_index = j;
+        }
+        if(oppmark_count == 2 && empty_index != -1)
+        {
+            game[empty_index][i] = mark;
+            return true;
+        }
+    }
+
+    oppmark_count = 0, empty_index = -1;
+    for (size_t i = 0; i < 3; i++)
+    {
+        if(game[i][i] == oppMark) oppmark_count ++;
+        else if (game[i][i] == ' ') empty_index = i;
+    }
+    if(oppmark_count == 2 && empty_index != -1)
+    {
+        game[empty_index][empty_index] = mark;
+        return true;
+    }
+
+    oppmark_count = 0, empty_index = -1;
+    for (size_t i = 0; i < 3; i++)
+    {
+        if(game[2 - i][i] == oppMark) oppmark_count ++;
+        else if (game[2 - i][i] == ' ') empty_index = 2 - i;
+        if(oppmark_count == 2 && empty_index != -1)
+        {
+            game[empty_index][i] = mark;
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void last_resort_placement(char game[][3], char mark)
+{
+    int mark_count = 0, empty_index = -1;
+    if (game[1][1] == ' ')
+    {
+        game[1][1] = mark;
+        return;
+    }else
+    {
+        for (size_t i = 0; i < 3; i++)
+        {
+            if (game[i][i] == ' ')
+            {
+                game[i][i] = mark;
+                return;
+            }
+            else if(game[2 - i][i] == ' ')
+            {
+                game[2 - i][i] = mark;
+                return;
+            }
+        }
+
+        for (size_t i = 0; i < 3; i++)
+        {
+            for (size_t j = 0; j < 3; j++)
+            {
+                if (game[i][j] == ' ')
+                {
+                    game[i][j] = mark;
+                    return;
+                }else if (game[j][i] == ' ')
+                {
+                    game[j][i] = mark;
+                    return;
+                }
+            }
+            
+        }
+        
+    }
+}
+
 // make_move()
 // Summary: This AI function makes a move on behalf of the computer in an ongoing tic-tac-toe game.
 // Arguments:
@@ -34,15 +193,22 @@ void ask_for_move(char game[][3], char mark){
 //           mark: The AI's mark: 'X' or 'O'.
 // Returns: Nothing.
 
-#define TWO_PLAYERS
 void make_move(char game[][3], char mark){ 
-    #ifdef TWO_PLAYERS
-    ask_for_move(game,mark);
-    #else
-    
     // Write your code here and comment out the definition of TWO_PLAYERS above
+    char oppMark = mark == 'X' ? 'O' : 'X';
+    
+    if (try_to_win(game, mark))
+    {
+        return;
+    }
+    
+    if (try_to_block(game, mark, oppMark))
+    {
+        return;
+    }
 
-    #endif
+    last_resort_placement(game, mark);
+
     return;
 }
 
@@ -56,10 +222,39 @@ void make_move(char game[][3], char mark){
 //                                  'O': O won.
 //                                  't': A tie.
 char game_state(char game[][3]){
+    int TotalSquares;
+    char gameStat = 't';
 
+    for (size_t i = 0; i < 3; i++)
+    {
+        for (size_t j = 0; j < 3; j++)
+        {
+            if (game[i][j] == ' ')
+            {
+                gameStat = 'a';
+            }
+            
+        }
+        
+    }
+    
     // Write your code here
+    for (size_t i = 0; i < 3; i++)
+    {
+        if(game[i][0] != ' ' && (game[i][0] == game[i][1]) && (game[i][0] == game[i][2]))
+        {
+            gameStat = game[i][0];
+        }else if(game[0][i] != ' ' && (game[0][i] == game[1][i]) && (game[0][i] == game[2][i])){
+            gameStat = game[0][i];
+        }
+        
+    }
 
-    return 'a';
+    if (game[1][1] != ' ' && (((game[0][0] == game[1][1]) && (game[1][1] == game[2][2])) || ((game[2][0] == game[1][1]) && (game[1][1] == game[0][2]))))
+    {
+        gameStat = game[1][1];
+    }
+    return gameStat;
 }
 
 // print_game()
